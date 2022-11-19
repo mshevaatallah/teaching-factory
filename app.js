14ba77b2c79b9d7491228ac8c0b0f425da0e3a6c
sxhvxa <muhammadshax07@gmail.com>
2022-11-19 19:01:33 +0700

    add particle

--- app.js
+++ app.js
@@ -1,22 +1,4 @@
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const container_login = document.querySelector(".container-login");
-const sign_in_btn2 = document.querySelector("#sign-in-btn2");
-const sign_up_btn2 = document.querySelector("#sign-up-btn2");
-
-sign_up_btn.addEventListener("click", () => {
-    container_login.classList.add("sign-up-mode");
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load("particles-js", "particles.json", function () {
+  console.log("callback - particles.js config loaded");
 });
-
-sign_in_btn.addEventListener("click", () => {
-    container_login.classList.remove("sign-up-mode");
-});
-
-sign_up_btn2.addEventListener("click", () => {
-    container_login.classList.add("sign-up-mode2");
-});
-
-sign_in_btn2.addEventListener("click", () => {
-    container_login.classList.remove("sign-up-mode2");
-});
-
